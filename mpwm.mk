a75493ae4564c1e360f5e0bda3eb2d4ef175a73b
user <0@void.re>
2022-06-04 13:18:26 +0200

    Bump version and remove pretty print stuff

--- mpwm.mk
+++ mpwm.mk
@@ -1,5 +1,5 @@
 # mpwm version
-VERSION = 1.1
+VERSION = 1.2
 
 # Compiler/linker
 CC              := /usr/bin/clang-14
@@ -11,7 +11,7 @@ SRCS            = $(wildcard src/*.c)
 HDRS            = $(wildcard src/*.h)
 
 # Auto files
-BUILD_TYPE      := Release
+BUILD_TYPE      := Unknown
 ROOT_OBJDIR     := .build
 ROOT_DEPDIR     := .deps
 OBJDIR          := ${ROOT_OBJDIR}/${BUILD_TYPE}
@@ -23,21 +23,12 @@ DEPS            = $(SRCS:%.c=${DEPDIR}/%.d)
 OBJDIRS         = $(shell dirname ${OBJS} | sort -u)
 DEPDIRS         = $(shell dirname ${DEPS} | sort -u)
 
-# Verbose stuff
-COL_GREEN       = \033[0;32m
-COL_YELLOW      = \033[1;33m
-COL_BLUE        = \033[0;34m
-COL_DEF         = \033[0m
-
-MSG_INFO        = [${COL_BLUE}*${COL_DEF}]
-MSG_PREV_LINE   = \033[F
-
 # Flags
 INCLUDES        = -I/usr/include/freetype2
 DEPFLAGS        = -MT $@ -MMD -MP -MF ${DEPDIR}/$*.d
-CFLAGS          := -O3 -march=native -pedantic -Wall -Wextra -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" -DXINERAMA ${INCLUDES}
+CFLAGS          := -O3 -march=native -pedantic -Wall -Wextra -flto=full -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=2 -DVERSION=\"${VERSION}\" -DXINERAMA ${INCLUDES}
 CFLAGS_WARNINGS := -Wno-unused-function -Wno-pointer-to-int-cast -Wno-unused-variable -Wno-division-by-zero
-LDFLAGS         := -lX11 -lXi -lXinerama -lfontconfig -lXft -lXrender
+LDFLAGS         := -flto=full -lX11 -lXi -lXinerama -lfontconfig -lXft -lXrender
 
 .PHONY: all clean
 
@@ -48,15 +39,11 @@ debug: ${TARGET}
 
 # Build target
 ${TARGET}: ${OBJS}
-	@echo "[${COL_YELLOW}LD${COL_DEF}] ${OBJS}"
-	@${CC} -o $@ $^ ${LDFLAGS}
-	@echo "${MSG_PREV_LINE}[${COL_GREEN}LD${COL_DEF}] ${OBJS} => $@"
+	${CC} -o $@ $^ ${LDFLAGS}
 
 # Generic source -> object target
 ${OBJDIR}/%.o: %.c | ${OBJDIRS} ${DEPDIRS}
-	@echo "[${COL_YELLOW}CC${COL_DEF}] $<"
-	@${CC} ${DEPFLAGS} -c ${CFLAGS} ${CFLAGS_WARNINGS} -o $@ $<
-	@echo "${MSG_PREV_LINE}[${COL_GREEN}CC${COL_DEF}] $< => $@"
+	${CC} ${DEPFLAGS} -c ${CFLAGS} ${CFLAGS_WARNINGS} -o $@ $<
 
 install: all
 	sudo mkdir -p "${DESTDIR}${PREFIX}/bin"
@@ -71,8 +58,7 @@ clean:
 	rm -f "${TARGET}"
 
 ${OBJDIRS} ${DEPDIRS}:
-	@echo "${MSG_INFO} mkdir -p $@"
-	@mkdir -p $@
+	mkdir -p $@
 
 DEPFILES = $(SRCS:%.c=${DEPDIR}/%.d)
 
