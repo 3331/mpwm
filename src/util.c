b9eeb1cb543f5262827d7af1200543bd7a7588d5
user <0@void.re>
2023-01-08 00:10:34 +0100

    Add some swap functions for other types

--- src/util.c
+++ src/util.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <unistd.h>
 
 #include "util.h"
@@ -39,9 +40,33 @@ die(const char *fmt, ...) {
 }
 
 void inline
+swap_float(float* a, float* b)
+{
+    float tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void inline
 swap_int(int* a, int* b)
 {
     int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void inline
+swap_uint32(uint32_t* a, uint32_t* b)
+{
+    uint32_t tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+
+void inline
+swap_void(void** a, void** b)
+{
+    void* tmp = *a;
     *a = *b;
     *b = tmp;
 }
--- src/util.h
+++ src/util.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <stdint.h>
+
 #define CLAMP(X, A, B)          (MIN(MAX(A, B), MAX(X, MIN(A, B))))
 #define BETWEEN(X, A, B)        ((A) <= (X) && (X) <= (B))
 
@@ -13,4 +15,7 @@ void die(const char *fmt, ...);
 void *ecalloc(size_t nmemb, size_t size);
 
 void swap_int(int* a, int* b);
+void swap_uint32(uint32_t* a, uint32_t* b);
+void swap_void(void** a, void** b);
+void swap_float(float* a, float* b);
 void swap_ulong(unsigned long* a, unsigned long* b);
